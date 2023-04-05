7beb39c3c74cfc779b483933ee45dfc874428b39
lave29 <dlaco1028@naver.com>
2023-04-05 15:36:36 +0900

    230405 중간 저장

---
+++ pycoding/function_230405.py
@@ -0,0 +1,35 @@
+def absum():
+    a=1
+    b=2
+    c=a+b
+    print('a=', a, 'b=', b, 'a+b=', c)
+
+def abminus(a, b):
+    # a=1
+    # b=2
+    c=a-b
+    print('a=', a, 'b=', b, 'a-b=', c)
+
+def abmulti():
+    a=1
+    b=2
+    c=a*b
+    print('a=', a, 'b=', b, 'a*b=', c)
+    return c
+
+def abdivide(a, b):
+    # a=1
+    # b=2
+    c=a/b
+    print('a=', a, 'b=', b, 'a-b=', c)
+    return c
+
+
+# absum()
+# abminus(3, 4)
+# print(abmulti())    #비추
+# c=abmulti()
+# print('a*b=', c)
+
+# c=abdivide(6,5)
+# print('a/b=', c)
---
+++ pycoding/main_230405.py
@@ -0,0 +1,25 @@
+# import math
+
+# c = math.cos(100)
+# print(c)
+
+# from math import exp
+
+# a=exp(100)
+# print(a)
+
+# import math as m
+# c = m.cos(100)
+# print(c)
+
+import function_230405 as f
+
+# f.absum()
+# f.abdivide(2, 3)
+
+def main():
+    f.absum()
+    f.abdivide(2, 3)
+
+if __name__ == '__main__':
+    main()
